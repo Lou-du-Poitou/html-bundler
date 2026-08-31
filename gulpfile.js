d7a63ad240938a18c5da53d9f8cd056ba7cfd8c8
V / Lou du Poitou <101828845+Lou-du-Poitou@users.noreply.github.com>
2026-08-31 12:00:39 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -284,7 +284,7 @@ function buildall() {
                     },
                     ontext(text) {
                         // Push text
-                        chunks.push(text);
+                        chunks.push(escape(text));
                     },
                     onclosetag(name, isImplied) {
                         if (!isImplied) {
